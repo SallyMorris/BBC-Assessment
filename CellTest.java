2c31e079186376c0110758b96434d7c612124a44
Sally Morris <sally.obi.morris@gmail.com>
2016-03-10 23:54:17 +0000

    Create CellTest.java

---
+++ CellTest.java
@@ -0,0 +1,50 @@
+package bbc.gameoflifestub;
+
+import java.util.HashSet;
+import org.junit.Test;
+import static org.junit.Assert.*;
+	
+public class CellTest {
+
+	public void testEquality()
+    {
+        Cell c1 = new Cell(1, 2);
+        Cell c2 = new Cell(1, 2);
+		assertEquals(c1, c2);
+	}
+
+	public void testInequality()
+    {
+        Cell c1 = new Cell(1, 2);
+        Cell c2 = new Cell(1, 3);
+		assertFalse(c1.equals(c2));
+
+        c1 = new Cell(0, 2);
+        c2 = new Cell(1, 2);
+        assertFalse(c1.equals(c2));
+	}
+
+    public void testHash()
+    {
+        assertEquals(new Cell(1, 1).hashCode(), new Cell(1, 1).hashCode());
+
+        HashSet<Cell> set = new HashSet<Cell>();
+        set.add(new Cell(1, 1));
+        set.add(new Cell(1, 1));
+        set.add(new Cell(2, 2));
+
+        assertEquals(2, set.size());
+    }
+
+
+    public void testSetsOfSameCellsAreEqual(){
+        HashSet<Cell> setOfCells = new HashSet<Cell>();
+        setOfCells.add(new Cell(1,1));
+
+        HashSet<Cell> setOfCells2 = new HashSet<Cell>();
+        setOfCells2.add(new Cell(1,1));
+
+        assertEquals(setOfCells, setOfCells2);
+    }
+
+}
